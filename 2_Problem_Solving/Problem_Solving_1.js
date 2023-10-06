47571d576cfb849b2ea23cd4632ddcd2ace8810d
Elvis Garcia <112880925+Coach-Elvis@users.noreply.github.com>
2023-10-06 14:45:32 -0400

    Update and rename Problem_Solving_1.js to Problem_Solving_1.txt

--- 2_Problem_Solving/Problem_Solving_1.js
+++
@@ -1,7 +0,0 @@
-/*
-  Use the SEAT Method to practice learning and describing your approach to solving the following problem
-
-  PROBLEM STATEMENT: 
-    - Given two numbers, write a function that will return their sum. 
-*/
-
