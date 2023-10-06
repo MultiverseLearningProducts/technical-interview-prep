388c9f7a8aa76b0079bb0d10b59355d8cd21d443
Elvis Garcia <112880925+Coach-Elvis@users.noreply.github.com>
2023-10-06 14:36:15 -0400

    Create Problem_Solving_1.js

---
+++ 2_Problem_Solving/Problem_Solving_1.js
@@ -0,0 +1,7 @@
+/*
+  Use the SEAT Method to practice learning and describing your approach to solving the following problem
+
+  PROBLEM STATEMENT: 
+    - Given two numbers, write a function that will return their sum. 
+*/
+
